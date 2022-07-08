8b531dde4fd4cd95f87a322d921e674de4d0bd73
piotr-mrocz <p.mrocz@vp.pl>
2022-07-08 13:04:36 +0200

    Check password

---
+++ IsPasswordContainsSpecialChar/Program.cs
@@ -0,0 +1,34 @@
+﻿var isStrongPassword = false;
+var possibleChars = new List<char>{'!', '@', '#', '$', '%', '^', '&', '*', '(', ')'};
+
+while (!isStrongPassword)
+{
+    Console.WriteLine("test");
+    Console.Write("Wprowadź hasło: ");
+    var password = Console.ReadLine();
+    var isPasswordContainsSpecialChar = password.Any(x => possibleChars.Contains(x));
+    
+    if (!password.Any(char.IsUpper))
+    {
+        Console.WriteLine("Nowe hasło musi zawierać przynajmniej 1 wielką literę!");
+    }
+    else if (!password.Any(char.IsDigit))
+    {
+        Console.WriteLine("Nowe hasło musi zawierać przynajmniej 1 cyfrę!");
+    }
+    else if (password.Trim().Length < 6)
+    {
+        Console.WriteLine("Nowe hasło musi zawierać przynajmniej 6 znaków!");
+    }
+    else if (!isPasswordContainsSpecialChar)
+    {
+        Console.WriteLine("Nowe hasło musi zawierać przynajmniej 1 znak specjalny!");
+    }
+    else
+    {
+        Console.WriteLine("Silne hasło");
+        isStrongPassword = true;
+    }
+}
+
+
